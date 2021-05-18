b4b37923fe535ad8cdd106ee2db9bf02d1c8ae03
Fillipy <57646927+Fillipyp@users.noreply.github.com>
2021-05-18 13:56:24 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
+
+addEventListener("scroll", function(scrolapagina){
+  console.log(scrolapagina)
+});
+
+
+const btn = document.querySelector(".btn-toggle");
+// Select the stylesheet <link>
+const theme = document.querySelector("#dark-theme");
+
+// Listen for a click on the button
+btn.addEventListener("click", function() {
+  // If the current URL contains "ligh-theme.css"
+  if (theme.getAttribute("href") == "css/darkmode/geral.css") {
+    // ... then switch it to "dark-theme.css"
+    theme.href = "css/lightmode/geral.css";
+
+  // Otherwise...
+  } else {
+    // ... switch it to "light-theme.css"
+    theme.href = "css/darkmode/geral.css";
+  }
+});
 // var transparent = true;
 // var big_image;
 
